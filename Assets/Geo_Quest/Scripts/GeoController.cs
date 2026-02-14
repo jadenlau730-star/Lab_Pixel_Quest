ada3361fe0a460bf5310b69c01ce4de93afed69a
jadenlau730-star <jaden.lau730@gmail.com>
2026-02-14 16:31:15 -0500

    Lab

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoController : MonoBehaviour
+{
+    string train = "hello";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+        string hi = "world";
+        Debug.Log(train + hi);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
