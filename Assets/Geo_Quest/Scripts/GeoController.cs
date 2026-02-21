b24008590d4499bb98ef75f9141007f0b93dc0b6
jadenlau730-star <jaden.lau730@gmail.com>
2026-02-21 16:28:44 -0500

    Lab

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -3,19 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GeoController : MonoBehaviour
-{
+{public int counter = 0;
     string train = "hello";
     // Start is called before the first frame update
-    void Start()
+   public void Start()
     {
+        counter = 99;
         Debug.Log("Hello World");
         string hi = "world";
         Debug.Log(train + hi);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Debug.Log(counter);
+        counter++;
+       if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
     }
 }
